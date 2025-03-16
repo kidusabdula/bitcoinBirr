a3a63591687bb00e8a0e8c03ad4895ee76cf72ce
Naol22 <Naolgirma21@gmail.com>
2025-03-16 18:26:58 +0300

    eighth commit

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,6 +2,7 @@ import Hero2 from "@/components/Hero2";
 import BitcoinEthiopia from "@/components/BitcoinEthiopiaorg";
 import WhatIsBitcoin from "@/components/WhatIsBitcoin";
 import StoreOfValue from "@/components/StoreOfValue";
+import AddValue from "@/components/AddValue";
 export default function About() {
   return (
     <div className="bg-white pt-16">
@@ -10,6 +11,7 @@ export default function About() {
       <BitcoinEthiopia />
       <WhatIsBitcoin />
       <StoreOfValue/>
+      <AddValue/>
     </div>
   )
 }
---
+++ app/media/page.tsx
@@ -0,0 +1,13 @@
+import Media2021 from "@/components/Media2021";
+import Media2022 from "@/components/Media2022";
+import Media2023 from "@/components/Media2023";
+
+export default function Lectures() {
+  return (
+    <div className="bg-white pt-16">
+        <Media2023 />
+        <Media2022 />
+        <Media2021 />
+    </div>
+  )
+}
